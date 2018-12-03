164f0e418b7eae6b5fc3ca17f2660e2aa9d5cf6b
Natan Fagundes <natan.fags@gmail.com>
2018-12-03 14:53:07 -0200

    {DB1Start} Add metodos tamanhoDeNome, transformaParaMaiuscula para aula

---
+++ src/br/com/db1/db1start/Nome.java
@@ -0,0 +1,14 @@
+package br.com.db1.db1start;
+
+public class Nome {
+	
+	public String transformaParaLetraMaiuscula(String nome) {
+		return nome.toUpperCase();
+	}
+
+	public int tamanhoDoNome(String nome) {
+		return nome.length();
+	}
+	
+	
+}
---
+++ test/br/com/bd1/db1start/NomeTest.java
@@ -0,0 +1,28 @@
+package br.com.bd1.db1start;
+
+import org.junit.Test;
+
+import br.com.db1.db1start.Nome;
+import junit.framework.Assert;
+
+public class NomeTest {
+
+	@Test
+	public void deveTransformarParaMaiuscula() {
+		Nome nome = new Nome();
+		String valorTranformado = nome.transformaParaLetraMaiuscula("maiko cunha");
+		Assert.assertEquals("MAIKO CUNHA", valorTranformado);
+		
+		System.out.println(valorTranformado);
+	}
+	
+	
+	@Test
+	public void deveRetornarTamanhoDoNome() {
+		Nome nome = new Nome();
+		int tamanhoDoNome = nome.tamanhoDoNome("MAIKO CUNHA");
+		Assert.assertEquals(11, tamanhoDoNome);
+		
+		System.out.println(tamanhoDoNome);
+	}
+}
